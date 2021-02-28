fa0456e64d394d657ca163f53b3f3d3e4433821e
sree-chikati <srechi453.fusdk12@gmail.com>
2021-02-27 20:01:10 -0800

    Updated forms.py with necessary forms

---
+++ songs_app/auth/forms.py
@@ -0,0 +1,40 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError
+from songs_app.models import User
+from songs_app import bcrypt
+
+#Forms: 
+# SignUpForm, LoginForm
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('Username',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    name = StringField('Full Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if not user:
+            raise ValidationError('No user with that username. Please try again.')
+
+    def validate_password(self, password):
+        user = User.query.filter_by(username=self.username.data).first()
+        if user and not bcrypt.check_password_hash(
+                user.password, password.data):
+            raise ValidationError('Password doesn\'t match. Please try again.')
--- songs_app/main/forms.py
+++ songs_app/main/forms.py
@@ -1,1 +1,44 @@
-# Create your forms here.
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, DateField, SelectField, SubmitField, TextAreaField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
+from wtforms.validators import DataRequired, Length, ValidationError
+from songs_app.models import Song, Artist, Genre, Playlist, User
+
+#Forms: 
+# SongFrom, ArtistForm, GenreForm, PlaylistForm
+
+
+class SongForm(FlaskForm):
+    """Form to create a song."""
+    title = StringField('Song Title',
+        validators=[DataRequired(), Length(min=1, max=80)])
+    date = DateField('Date Released')
+    artist = QuerySelectField('Artist',
+        query_factory=lambda: Artist.query, allow_blank=False)
+    genres = QuerySelectMultipleField('Genres',
+        query_factory=lambda: Genre.query, allow_blank=False)
+    playlists = QuerySelectMultipleField('Playlist',
+        query_factory=lambda: Genre.query, allow_blank=False)
+    submit = SubmitField('Submit')
+
+
+class ArtistForm(FlaskForm):
+    """Form to create an Artist."""
+    name = StringField('Artist Name',
+        validators=[DataRequired(), Length(min=3, max=80)])
+    about = TextAreaField('Information About Artist')
+    submit = SubmitField('Submit')
+
+
+class GenreForm(FlaskForm):
+    """Form to create a genre."""
+    name = StringField('Genre Name',
+        validators=[DataRequired(), Length(min=3, max=80)])
+    submit = SubmitField('Submit')
+
+
+class PlaylistForm(FlaskForm):
+    """Form to create a playlist."""
+    name = StringField('Genre Name',
+        validators=[DataRequired(), Length(min=3, max=80)])
+    submit = SubmitField('Submit')
