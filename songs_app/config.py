0b8ee99fdc535ff746fe3a097e2aa8697399d92e
sree-chikati <srechi453.fusdk12@gmail.com>
2021-03-02 15:24:48 -0800

    Deploying database

--- songs_app/config.py
+++ songs_app/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 class Config(object):
     """Set environment variables."""
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('postgres://clywduorbpvohe:f59934b277a0c99afb650791cede50c47f10eb2afe4c6ad2bb61448ec049c0e7@ec2-54-164-238-108.compute-1.amazonaws.com:5432/d5b7opmg078g79')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
